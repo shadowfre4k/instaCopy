fdfea813c8c4db14a0cd17b822d09d9550e05ff3
Vincent Jacobs <86565286+shadowfre4k@users.noreply.github.com>
2024-03-15 20:04:59 -0700

    added tabs and photo section.more spacing and arrangement

--- instaCopy/src/components/Tabs.jsx
+++ instaCopy/src/components/Tabs.jsx
@@ -1,7 +1,14 @@
+import "./Tabs.css"
+
 export default function Tabs(){
     return(
-        <div>
-           <h1>post reel and tags here</h1>
-        </div>
+       <div className="lineAndContainer">
+            <hr className="lineBreak"></hr>
+            <div className="tabContainer">
+                <div><p>Posts</p></div>
+                <div><p>Reels</p></div>
+                <div><p>Tagged</p></div>
+            </div>
+       </div>
     )
 }
